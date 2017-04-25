da3105c987ea79447ebf545d26fd2c329a003376
huawillian <huawillian@gmail.com>
2017-04-25 12:37:49 -0400

    problem with cors

--- index.js
+++ index.js
@@ -3,17 +3,49 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 const FirebaseREST = require('firebase-rest').default;
 var jsonClient = new FirebaseREST.JSONClient('https://studyguidemvp.firebaseio.com/');
-var cors = require('cors');
 
 var app = express();
 
-app.use(cors())
 app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 app.use(bodyParser.json());
-app.use(express.static('build/bundled'))
+
+
+
+
+app.all('*', function(req, res,next) {
+    /**
+     * Response settings
+     * @type {Object}
+     */
+    var responseSettings = {
+        "AccessControlAllowOrigin": req.headers.origin,
+        "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+        "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+        "AccessControlAllowCredentials": true
+    };
+
+    /**
+     * Headers
+     */
+    res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+    res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+    res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+    res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+
+
+});
+
 
 // CURRENTLY HOSTED ON HEROKU https://secure-scrubland-91570.herokuapp.com
 // e.g. https://secure-scrubland-91570.herokuapp.com/api/cards
